f1fc8c5e60650faed0d616329ce1fb1dd0faa143
Xyntril <artemsaidulin82@gmail.com>
2026-09-16 21:42:40 +0500

    feat: add GPLv3 license, PWA support, and Pages workflow

---
+++ web/sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'shizo95-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './engine.js',
+  './Galmuri9.woff2',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
